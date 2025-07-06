afa311b3e5f5ca553067779e13b86317a84fc31b
HuntOreo <hloreair@outlook.com>
2025-07-05 22:25:06 -0400

    Completed exercise 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let result = '';
 
+  try {
+    if (num < 0) {
+      throw new Error('ERROR');
+    };
+
+    for (let i = 0; i < num; i++) {
+      result += string;
+    };
+  } catch (error) {
+    return error.message;
+  };
+
+  return result;
 };
 
 // Do not edit below this line
