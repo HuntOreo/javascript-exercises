3fb25a64df4481fd16a87873769d99c5a20e7937
HuntOreo <hloreair@outlook.com>
2025-07-05 23:48:02 -0400

    Complete exercise 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahren) {
+  const result = (fahren - 32) * (5 / 9);
+  const roundedResult = Math.round(result * 10) / 10;
+  return roundedResult;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  const result = celsius * (9 / 5) + 32;
+  const roundedResult = Math.round(result * 10) / 10;
+  return roundedResult;
 };
 
 // Do not edit below this line
