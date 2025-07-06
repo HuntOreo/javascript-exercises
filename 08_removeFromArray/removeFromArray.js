963932cb01fc195ee302e2e23a4c757f9456f4dc
HuntOreo <hloreair@outlook.com>
2025-07-06 01:04:29 -0400

    Redo exercise 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,16 +1,19 @@
-const removeFromArray = function (arr, ...valsToDelete) {
+const removeFromArray = function (arr, ...vals) {
+
+  // takes an array and some values
+  // finds the values inside of the array
+  // return an array without the provided values
+
   let result = [];
 
-  result = arr.filter(item => {
-    if (valsToDelete.includes(item)) {
-      return false;
+  arr.forEach(elem => {
+    if (!vals.includes(elem)) {
+      result.push(elem);
     };
-
-    return true;
   });
 
   return result;
-};
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
