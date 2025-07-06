6d89ac208d34401b02f080acc0b14c63b349ea1f
HuntOreo <hloreair@outlook.com>
2025-07-05 23:16:50 -0400

    Struggle through 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...valsToDelete) {
+  let result = [];
+
+  result = arr.filter(item => {
+    if (valsToDelete.includes(item)) {
+      return false;
+    };
+
+    return true;
+  });
+
+  return result;
 };
 
 // Do not edit below this line
