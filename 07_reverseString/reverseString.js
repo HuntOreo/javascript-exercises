6aaa601f18dff7763e8b5fa73a4ca75efebc596f
HuntOreo <hloreair@outlook.com>
2025-07-05 22:33:34 -0400

    Complete exercise 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  let stringDissected = string.split('');
+  let stringReversed = '';
 
+  stringDissected.reverse();
+  stringReversed = stringDissected.join('');
+
+  return stringReversed;
 };
 
 // Do not edit below this line
