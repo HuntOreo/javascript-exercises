dacda24bdeb09026f9aad23457f5f9bc86cb7ed1
HuntOreo <hloreair@outlook.com>
2025-07-05 23:30:27 -0400

    Complete exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,35 @@
-const sumAll = function() {
+const sumAll = function (intOne, intTwo) {
+  let result = 0;
+  let start = intOne;
+  let end = intTwo;
 
+  try {
+    if (intOne < 0 || intTwo < 0) {
+      throw new Error('ERROR');
+    };
+
+    if (typeof intOne != 'number' || typeof intTwo != 'number') {
+      throw new Error('ERROR');
+    };
+
+    if (!Number.isInteger(intOne) || !Number.isInteger(intTwo)) {
+      throw new Error('ERROR');
+    }
+
+    if (start > end) {
+      start = intTwo;
+      end = intOne;
+    };
+
+    for (; start <= end; start++) {
+      result += start;
+    };
+
+    return result;
+
+  } catch (error) {
+    return error.message;
+  };
 };
 
 // Do not edit below this line
