aa365afc08b937e7662864805c245e877ead1821
HuntOreo <hloreair@outlook.com>
2025-07-15 23:26:15 -0400

    Complete exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  if (arr.length == 0) {
+    return 0;
+  }
+
+  return arr.reduce((prev, current) => prev + current);
 };
 
-const multiply = function() {
+const multiply = function (arr) {
+  if (arr.length == 0) {
+    return 0;
+  }
 
+  return arr.reduce((prev, current) => prev * current);
 };
 
-const power = function() {
-	
+const power = function (int, power) {
+  return int ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function (int) {
+  let arr = [];
+  let result;
+  if (int == 0 || int == 1) {
+    return 1;
+  }
+
+  for (let i = 1; i <= int; i++) {
+    arr.push(i);
+  }
+
+  result = arr.reduce((prev, current) => prev * current);
+
+  return result;
 };
 
 // Do not edit below this line
