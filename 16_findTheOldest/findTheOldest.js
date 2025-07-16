aa8e4ba200de360f63178fbe882f26765b42a863
HuntOreo <hloreair@outlook.com>
2025-07-16 01:11:00 -0400

    Complete 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  people.sort((a, b) => {
+    let aAge = a.yearOfBirth - a.yearOfDeath;
+    let bAge = b.yearOfBirth - b.yearOfDeath;
+    const currentYear = new Date(Date.now()).getFullYear();
 
+    if (a.yearOfDeath === undefined) {
+      aAge = a.yearOfBirth - currentYear;
+    }
+
+    if (b.yearOfDeath === undefined) {
+      bAge = b.yearOfBirth - currentYear;
+    }
+
+
+    return aAge - bAge;
+  })
+
+  return people[0];
 };
 
 // Do not edit below this line
